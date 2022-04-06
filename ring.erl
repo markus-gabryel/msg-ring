3fbe002321bbd2949c6903e06fa283c0ae70bd62
Markus Gabryel Mello da Silva <69471356+markus-gabryel@users.noreply.github.com>
2022-04-05 23:16:02 -0400

    Limit the forwarding
    Just forward the message X times around the ring

--- ring.erl
+++ ring.erl
@@ -1,30 +1,45 @@
 -module(ring).
--export([create/2, start/1, loop/1]).
+-export([launch/3, create/3, first_loop/2, loop/1]).
 
-create(Count, Message) ->
-	Pid = spawn(ring, start, [Message]),
+% Launch the creation chain
+launch(Count, Message, Times) ->
+	Pid = spawn(ring, first_loop, [Message, Times]),
 	create(Count - 1, [Pid], Pid).
 
+% If the creation count is done
 create(0, [Prev | Rest], First) ->
 	First ! Prev,
 	Ring = [Prev | Rest],
 	lists:reverse(Ring);
 
+% Just create another process
 create(Count, [Prev | Rest], First) ->
 	Pid = spawn(ring, loop, [Prev]),
 	create(Count - 1, [Pid , Prev | Rest], First).
 
-start(Message) ->
+% Wait for the last process created
+first_loop(Message, Count) ->
 	receive
 		Next ->
-			Next ! Message,
+			% Start to forward the message
+			Next ! {Message, Count},
 			loop(Next)
 	end.
 
 loop(Next) ->
 	receive
-		Message ->
-			io:format("~p: ~s~n", [self(), Message]),
-			Next ! Message,
+		% If the forwarding count is done
+		{Message, 1} ->
+			% Print the message
+			io:format("* ~p: ~s~n", [self(), Message]),
+			loop(Next);
+
+		% If Count =/= 1
+		{Message, Count} ->
+			% Just forward the message
+			io:format("* ~p~n", [self()]),
+			Next ! {Message, Count - 1},
 			loop(Next)
 	end.
+
+
